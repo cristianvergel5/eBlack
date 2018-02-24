36916edd1eab6a9d5ffec648af40fa833f726128
Camilo <kmilo9713.cl@gmail.com>
2018-02-23 22:49:21 -0500

    Leer archivo de texto extension .eb

--- eBlack.py
+++ eBlack.py
@@ -1,1 +1,15 @@
-print("eBlack")
+def abrirArch_eBlack(name):
+
+	if name.find(".eb") == False:
+		name = name + ".eb"
+
+	try:
+
+		with open(name, "r") as archivo:
+			for line in archivo:
+				print(line)
+
+	except:
+		print("No existe un archivo eBlack con ese nombre, asegurese de que tenga extension .eb")
+
+abrirArch_eBlack("nuevo.eb")
