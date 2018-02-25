386ef5371075a6016929acb809f3ff3b9138cf42
Camilo <kmilo9713.cl@gmail.com>
2018-02-24 19:08:00 -0500

    Agrego funcion obtener Token con su tipo y lexema, añado algunas palabras reservadas en el Diccionario

--- eBlack.py
+++ eBlack.py
@@ -1,4 +1,4 @@
-def abrirArch_eBlack(name):
+def leer_eBlack(name):
 
 	if name.find(".eb") == False:
 		name = name + ".eb"
@@ -12,4 +12,26 @@ def abrirArch_eBlack(name):
 	except:
 		print("No existe un archivo eBlack con ese nombre, asegurese de que tenga extension .eb")
 
-abrirArch_eBlack("prueba.eb")
+def obtenerToken(simbolo):
+
+	simbolo = simbolo.lower() +  "\n"
+	tipo = ""
+
+	try:
+
+		with open("Dictionary_eBlack.eb", "r") as diccionario:
+			
+			for linea in diccionario:
+				if str(linea) == "PALABRA RESERVADA\n" or str(linea) == "OPERADOR\n":
+					tipo = linea
+
+				else:
+					if linea == simbolo:
+						tipo = tipo.rstrip("\n")
+						simbolo = simbolo.rstrip("\n")
+						return "["+tipo+", "+simbolo+"]"		
+
+	except:
+		print("Ha ocurrido un error, puede que sea el archivo diccionario de eBlack, revisar")
+				
+#abrirArch_eBlack("Dictionary_eBlack.eb")
