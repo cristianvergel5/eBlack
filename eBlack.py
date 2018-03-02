b19044095bb284959448bc7f85dcfb8e1344719d
Camilo <kmilo9713.cl@gmail.com>
2018-03-02 10:34:46 -0500

    modificacion expresiones regulares

--- eBlack.py
+++ eBlack.py
@@ -2,27 +2,30 @@ import win32com.client as clwin
 import re
 
 operadores = {'SUMA':'+', 'RESTA':'-', 'MULTIPLICACION':'*', 'DIVISION': '/', 'ASIGNACION': '~', 'MENOR QUE': '<',
-'MAYOR QUE':'>', 'IGUAL':'~~', 'MENOR IGUAL':'<~', 'MAYOR IGUAL':'>~', 'DIFERENTE':'|~'}
+'MAYOR QUE':'>', 'IGUAL':'~~', 'MENOR IGUAL':'<~', 'MAYOR IGUAL':'>~', 'DIFERENTE':'|~', 'MODULO':'%',
+'LLAVE IZQUIERDA':'{', 'LLAVE DERECHA':'}', 'COMA':',', 'PARENTESIS IZQUIERDO':'(', 'PARENTESIS DERECHO':')'}
 
 tokens = ['ID', 'NUMERO', 'SUMA', 'RESTA', 'MULTIPLICACION', 'DIVISION', 'PARENTESISI', 'PARENTESISD']
 
-puntuacion = {'PUNTO':'.', 'COMA':',', 'PUNTO Y COMA':';', 'DOS PUNTOS': ':', 'PUNTOS SUSPENSIVOS':'...',
-'PARENTESIS IZQUIERDO':'(', 'PARENTESIS DERECHO':')', 'CORCHETE IZQUIERDO':'[', 'CORCHETE DERECHO':']', 'BARRA':'',
-'LLAVE IZQUIERDA':'{', 'LLAVE DERECHA':'}'}
+puntuacion = {'PUNTO':'.', 'COMA':',', 'PUNTO Y COMA':';', 'DOS PUNTOS': ':', 'PUNTOS SUSPENSIVOS':'...', 'CORCHETE IZQUIERDO':'[', 'CORCHETE DERECHO':']'}
 
-reservadas = ['itr', 'miq', 'si', 'sino', 'entonces', 'retorna', 'clase', 'prueba', 'ajuste', 'verdad', 'falso', 'casos']
+reservadas = ['itr', 'miq', 'si', 'sino', 'entonces', 'retorna', 'clase', 'prueba', 'ajuste', 'verdad', 'falso', 'casos', 'escriba',
+'funcion']
 
 def SapiLee(lectura):
 	habla = clwin.Dispatch("SAPI.SpVoice")
 	habla.Speak(lectura)
 
+def identificarReservada(identificar):
+	
+
 def identificarID(identificar):
-	patron = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
+	patron = re.compile(r'[&][a-zA-Z_][a-zA-Z0-9_]*')
 
 	identificar = identificar.lower()
 	
 	if identificar not in operadores.values() and identificar not in reservadas and patron.search(identificar) != None:
-		return "[ID, "+identificar+"]"
+		return "[IDENTIFICADOR, "+identificar+" ]"
 
 	return False
 
@@ -34,29 +37,37 @@ def identificarComentario(identificar):
 		return True
 
 	return False
+
+def identificarString(identificar):
+	patron = re.compile(r'\".*\"')
+
+	if patron.search(identificar) != None:
+		return True
+
+	return False
+
+def identificarNumero(identificar):
+
+	flotante = identificar
+	flotante = flotante.replace(",", "")
+	flotante = flotante.replace(".", "")
+
+	if identificar.isdigit() == True:
+		return "[ENTERO, "+identificar+" ]"
+	elif flotante.isdigit() == True:
+		return "[FLOTANTE, "+identificar+" ]"
+
+	return False
 	
 class lexer_eBlack():
 
-	def __init__(self, dictionary, filename):
-		self.__dictionary = dictionary
+	def __init__(self, filename):
 		self.__filename = filename
 		self.__tokens = []
 
 	@property
-	def dictionary(self):
-		return self.__dictionary
-
-	@dictionary.setter
-	def dictionary(self, dictionary):
-		self.__dictionary = dictionary
-
-	@dictionary.deleter
-	def dictionary(self):
-		self.__dictionary = None
-
-	@property
 	def filename(self):
-		return self.__dictionary
+		return self.__filename
 
 	@filename.setter
 	def filename(self, filename):
@@ -82,17 +93,21 @@ class lexer_eBlack():
 
 		simbolo = simbolo.lower()
 		simbolo = simbolo.rstrip("\n")
+		simbolo = simbolo.rstrip("\t")
+		simbolo = simbolo.replace(" ", "")
 		parecido = ""
 		tipo = ""
 
 		try:
 
 			if simbolo in reservadas:
-				return "["+"PALABRA RESERVADA, "+simbolo+"]"
+				return "["+"PALABRA RESERVADA, "+simbolo+" ]"
 			elif simbolo in operadores.values():
-				return "["+"OPERADOR, "+simbolo+"]"
+				return "["+"OPERADOR, "+simbolo+" ]"
 			elif identificarID(simbolo) != False:
 				return identificarID(simbolo)
+			elif identificarNumero(simbolo) != False:
+				return identificarNumero(simbolo)
 
 			return None
 
@@ -114,7 +129,7 @@ class lexer_eBlack():
 					for linea in archivo:
 						
 						lista = linea.split(" ")
-
+						
 						for i in lista:
 
 							if identificarComentario(i) != True:
@@ -134,15 +149,16 @@ class lexer_eBlack():
 			except:
 				print("Ha ocurrido un error en el proceso de lectura del programa eBlack")
 
+print(identificarString("Cadena de caracteres"))
 
 SapiLee("Inicio de lexer eBlack")
-eBlack = lexer_eBlack("Dictionary_eBlack.eb", "prueba.eb")
+eBlack = lexer_eBlack("prueba.eb")
 eBlack.leerPrograma()
 Resultado = eBlack.tokens
 
 for i in Resultado:
 	print(i)
-	SapiLee(i)
+#	SapiLee(i)
 
 SapiLee("Fin de lexer eBlack")
 #abrirArch_eBlack("Dictionary_eBlack.eb")
